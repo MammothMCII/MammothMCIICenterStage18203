72b5461827c68f7fb7720c0ceb5d7a06e0904dfa
MCII <mammothmcii@gmail.com>
2024-02-06 19:21:24 -0800

    worked on april tag detection for autonomous andd added a little snap to the backboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMain.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import static androidx.core.math.MathUtils.clamp;
+
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -9,6 +13,16 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.checkerframework.checker.units.qual.A;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+
 
 @TeleOp(name = "TeleopMain")
 public class TeleopMain extends LinearOpMode {
@@ -27,6 +41,10 @@ public class TeleopMain extends LinearOpMode {
     private Servo hand_tilt;
     private Servo bottom_grip;
     private DcMotor arm_slide;
+
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
 
 
 
@@ -54,7 +72,7 @@ public class TeleopMain extends LinearOpMode {
         boolean tiltToggle = false;
         boolean tiltOn = false;
 
-
+        initAprilTag();
 
         topleftmotor = hardwareMap.get(DcMotor.class, "top left motor");
         bottomleftmotor = hardwareMap.get(DcMotor.class, "bottom left motor");
@@ -81,25 +99,17 @@ public class TeleopMain extends LinearOpMode {
         top_grip.setPosition(1);
 
 
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
+                telemetryAprilTag();
+
+                telemetry.update();
+
                 drive_speed = (0.5 + (gamepad1.right_trigger/2) - (gamepad1.left_trigger/3));
 
-                if (gamepad1.touchpad_finger_1_x > 0) {
-                    // touchpad arm controlls, very cool
-                    //todo: make this work for two fingers if this is actually what we do
-                    arm_power = -gamepad1.touchpad_finger_1_y/4;
-                }
-
-                if (gamepad1.touchpad_finger_1_x < 0) {
-                    arm_tilt_speed = -gamepad1.touchpad_finger_1_y/3;
-                }
-
-                if (!gamepad1.touchpad_finger_1){
-                    arm_power = 0;
-                    arm_tilt_speed = 0;
-                }
 
                 // this handles macanum wheel driving with strafe
                 // multiplied by drive speed
@@ -118,15 +128,27 @@ public class TeleopMain extends LinearOpMode {
                     winch.setPower(0);
                 }
 
-                if (gamepad1.b && !armlimitbutton.isPressed()) {
-                    arm_slide.setPower(1);
+                arm_power = gamepad1.left_stick_y;
+                if (arm_power > 0.2) {
+                    clamp(arm_power, 0.2, 1);
                 }
-                else if (gamepad1.a && !armsafetybutton.isPressed()) {
-                    arm_slide.setPower(-1);
+                else if (arm_power < 0.2) {
+                    clamp(arm_power, -0.2, -1);
                 }
                 else{
-                    arm_slide.setPower(0);
+                    arm_power = 0;
                 }
+
+                if (armsafetybutton.isPressed()) {
+                    arm_slide.setPower(Math.min(Math.max(-arm_power, 0), 1));
+                }
+                else if(armlimitbutton.isPressed()) {
+                    arm_slide.setPower((Math.min(Math.max(-arm_power, -1), 0)));
+                }
+                else {
+                    arm_slide.setPower(-arm_power);
+                }
+
 
                 if (gamepad1.dpad_left && !winchToggle) {
                     if (winchOn == false) {winch_release.setPosition(1); winchOn = true;}
@@ -180,21 +202,7 @@ public class TeleopMain extends LinearOpMode {
                 }
 
 
-                /**
-                //this will be the real stuff
-                if (armsafetybutton.isPressed()) {
 
-                    Arm.setPower(Math.min(Math.max(arm_power, -1), 0));
-                }
-                else if(armlimitbutton.isPressed()) {
-
-                    Arm.setPower((Math.min(Math.max(arm_power, 0), 1)));
-                }
-                else {
-
-                    Arm.setPower(arm_power);
-                }
-                **/
 
                 if (gamepad1.dpad_right && !planeToggle) {
                     if (planeOn == false) {planeLauncher.setPosition(1); planeOn = true;}
@@ -207,7 +215,9 @@ public class TeleopMain extends LinearOpMode {
                 else if (!gamepad1.dpad_right) planeToggle = false;
 
 
-
+                if (gamepad1.a){
+                    lockToBackdrop();
+                }
 
 
                 telemetry.update();
@@ -236,5 +246,126 @@ public class TeleopMain extends LinearOpMode {
         toprightmotor.setPower(0);
 
     }
+
+    private void initAprilTag() {
+
+        // Create the AprilTag processor.
+        aprilTag = new AprilTagProcessor.Builder()
+
+                // The following default settings are available to un-comment and edit as needed.
+                //.setDrawAxes(false)
+                //.setDrawCubeProjection(false)
+                //.setDrawTagOutline(true)
+                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                .setLensIntrinsics(1430 , 1430 , 480, 620)
+                // ... these parameters are fx, fy, cx, cy.
+
+                .build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        //aprilTag.setDecimation(3);
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "FrontCamera"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        builder.setCameraResolution(new Size(1280, 720));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        //builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(aprilTag);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        //visionPortal.setProcessorEnabled(aprilTag, true);
+
+    }   // end method initAprilTag()
+
+
+    /**
+     * Add telemetry about AprilTag detections.
+     */
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        //telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        //telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        //telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }   // end method telemetryAprilTag()
+
+    private void lockToBackdrop() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        double average_offset = 0;
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                //telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                //telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                //telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                average_offset += detection.ftcPose.yaw;
+            }
+        }   // end for() loop
+        average_offset = average_offset/currentDetections.size();
+
+        bottomleftmotor.setPower(-average_offset/20);
+        topleftmotor.setPower(-average_offset/20);
+        bottomrightmotor.setPower(average_offset/20);
+        toprightmotor.setPower(average_offset/20);
+
+
+
+
+    }   // end method telemetryAprilTag()
+
+
 
 }
